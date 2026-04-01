b168a39144645db1f66eac6dd7981f760f7bbce1
Olivier-Gendron <2538864@cegeplimoilou.ca>
2026-04-01 09:47:40 -0400

    Implémentation méthode creationListeFichiers

--- APP4-OlivierGendron/src/app/CircuitApp.java
+++ APP4-OlivierGendron/src/app/CircuitApp.java
@@ -2,16 +2,35 @@ package app;
 
 import electronique.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CircuitApp {
+    private static final char fSep = File.separatorChar;
+    private static final String pathIn = System.getProperty("user.dir") + fSep + "APP4-OlivierGendron" + fSep + "src" + fSep + "donnees" + fSep + "fichiers_json";
+
+    private List<String> fichiersDisponibles = new ArrayList<>();
 
     public CircuitApp() {
 
     }
 
     static void main() {
+        System.out.println();
+    }
+
+    private void creationListeFichiers() {
+
+        try {
+            Files.list(Path.of(pathIn));
+        } catch (IOException e) {
+            System.err.println("Package vide: Aucun fichier disponible");
+        }
+
 
     }
 }
--- APP4-OlivierGendron/src/app/CircuitBuilder.java
+++ APP4-OlivierGendron/src/app/CircuitBuilder.java
@@ -14,10 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CircuitBuilder {
-    private static final char fSep = File.separatorChar;
-    private static final String pathIn1 = System.getProperty("user.dir") + fSep + "APP4-OlivierGendron" + fSep + "src" + fSep + "fichiers_json" + fSep + "complexe_industriel_zone_nord.json";
-    private static final String pathIn2 = System.getProperty("user.dir") + fSep + "APP4-OlivierGendron" + fSep + "src" + fSep + "fichiers_json" + fSep + "eclairage_public_quartier.json";
-    private static final String pathIn3 = System.getProperty("user.dir") + fSep + "APP4-OlivierGendron" + fSep + "src" + fSep + "fichiers_json" + fSep + "reseau_secours_hopital.json";
+
 
     public CircuitBuilder() {
 
